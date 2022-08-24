2d7df717a8f2a369402bffcbc4fef71453f060ce
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-08-24 14:28:26 +0700

    add static django

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    email = models.EmailField(max_length=25)
+
+    class Meta:
+        db_table = "users"
--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from crud.models import Users
 
 # Create your views here.
+
+
+def show(request):
+    users = Users.objects.all()
+    template = loader.get_template('show.html')
+    return HttpResponse(template.render({'users': users}))
