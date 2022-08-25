72ed4b6beb7c2496b3428636bba15879c7c52507
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-08-25 17:03:55 +0700

    add insert menthod

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.template import loader
+from django.template import loader, RequestContext
 from django.http import HttpResponse
 from crud.models import Users
 
@@ -12,7 +13,19 @@ def show(request):
     return HttpResponse(template.render({'users': users}))
 
 
+@csrf_exempt
 def insert(request):
+
+    if request.method == "POST":
+        id = request.POST['id']
+        user_id = request.POST['user_id']
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        data = Users(id=id, user_id=user_id, username=username,
+                     firstname=firstname, lastname=lastname, email=email)
+        data.save()
 
     template = loader.get_template('insert.html')
     return HttpResponse(template.render())
