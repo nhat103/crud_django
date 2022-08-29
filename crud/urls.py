b4182cab8153ff72962d4858bbeabd8ecd64496a
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-08-29 21:00:49 +0700

    format code

--- crud/urls.py
+++ crud/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('info/', views.info, name='infouser'),
     path('insert/', views.insert, name='insertuser'),
-    path('choice/', views.choiceUser, name='edit'),
+    path('choice/', views.choiceUser, name='edituser'),
     path('edit/', views.update, name='update'),
     path('edit/<int:pk>', views.edit, name='edit'),
     path('delete/<int:pk>', views.deleteUser, name='delete'),
